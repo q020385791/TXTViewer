31e3fb3f7ac53daf9a990b61897ff4443b1ea45d
sai wanza <q020385791@gmail.com>
2021-03-29 17:10:16 +0800

    加入專案檔案。

---
+++ SchemaSearch/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace SchemaSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtViewer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.treeView1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.treeView1.ForeColor = System.Drawing.SystemColors.Info;
+            this.treeView1.LineColor = System.Drawing.Color.White;
+            this.treeView1.Location = new System.Drawing.Point(13, 70);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(243, 651);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtSearch.Location = new System.Drawing.Point(13, 26);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(242, 29);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // txtViewer
+            // 
+            this.txtViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtViewer.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtViewer.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtViewer.Location = new System.Drawing.Point(285, 26);
+            this.txtViewer.Multiline = true;
+            this.txtViewer.Name = "txtViewer";
+            this.txtViewer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtViewer.Size = new System.Drawing.Size(1174, 695);
+            this.txtViewer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1471, 733);
+            this.Controls.Add(this.txtViewer);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "SQLFileViewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtViewer;
+    }
+}
+
---
+++ SchemaSearch/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchemaSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string SourceFolderPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+        public string SourceConcatTableScript = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SourceConcatTableScript = SourceFolderPath+"\\Schema";//指定資料夾名稱要為"Schema"
+            GetTreeViewByText("");
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string[] FilesName = Directory.GetFiles(SourceConcatTableScript);
+            string SelectedText= treeView1.SelectedNode.Text;
+            string TargetPath = SourceConcatTableScript + "\\" + SelectedText+".txt";
+            if (File.Exists(TargetPath))
+            {
+                Encoding unicode = Encoding.GetEncoding(950);
+                try
+                {
+                    txtViewer.Text = File.ReadAllText(TargetPath, unicode);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法讀取:"+ TargetPath+"請確認檔案"+Environment.NewLine+ex.InnerException);
+                }
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            txtSearch.Text = txtSearch.Text.ToUpper();
+            txtSearch.SelectionStart = txtSearch.Text.Length;
+            txtSearch.SelectionLength = 0;
+            GetTreeViewByText(txtSearch.Text);
+        }
+        public void GetTreeViewByText(string SearchText) 
+        {
+            treeView1.Nodes.Clear();
+            string[] FilesName = Directory.GetFiles(SourceConcatTableScript);
+            Dictionary<string, List<string>> Dic = new Dictionary<string, List<string>>();
+            foreach (string FullFilePath in FilesName)
+            {
+                string FileName = Path.GetFileName(FullFilePath).Split('.')[0];
+                if (FileName.Contains(SearchText)|| SearchText=="")
+                {
+                    string SubTitleName = FileName.Substring(0, 3);
+                    if (!Dic.Keys.Contains(SubTitleName))
+                    {
+                        //如果是全新目錄
+                        List<string> ListChildren = new List<string>();
+                        ListChildren.Add(FileName);
+                        Dic.Add(SubTitleName, ListChildren);
+                    }
+                    else
+                    {
+                        // 如果已存在目錄
+                        Dic[SubTitleName].Add(FileName);
+                    }
+                }
+            }
+            foreach (var items in Dic)
+            {
+                string Title = items.Key.Substring(0, 3).ToUpper(); ;//標題取三個字元 大寫
+                TreeNode ParentNode = new TreeNode(Title);
+                foreach (var item in items.Value)
+                {
+                    TreeNode ChildrenNode = new TreeNode(item);
+                    ParentNode.Nodes.Add(ChildrenNode);
+                }
+                treeView1.Nodes.Add(ParentNode);
+            }
+        }
+    }
+}
