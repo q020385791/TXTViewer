ed6ca6420e00c931d1b4f06030fffe97e377fc8b
sai wanza <q020385791@gmail.com>
2021-03-29 17:16:15 +0800

    修改標頭

--- SchemaSearch/Form1.Designer.cs
+++ SchemaSearch/Form1.Designer.cs
@@ -82,7 +82,7 @@ namespace SchemaSearch
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
-            this.Text = "SQLFileViewer";
+            this.Text = "TXTFileViewer";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
